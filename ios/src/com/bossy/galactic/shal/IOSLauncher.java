e990bebafcb1ff32737eb04883de0a806555fd2a
Shailesh <shaileshpant23@gmail.com>
2019-12-28 15:04:39 +0530

    Created new project for a game

---
+++ ios/src/com/bossy/galactic/shal/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.bossy.galactic.shal;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.bossy.galactic.shal.Galaxy;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new Galaxy(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
